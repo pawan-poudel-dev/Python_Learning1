f9d3e1fbe0cff54306b64041c0940f7bf36306a2
pawan_poudel_Devloper <poudelpawan770@gmail.com>
2026-02-24 17:31:25 +0545

    Add files via upload

---
+++ InputNumbers.py
@@ -0,0 +1,6 @@
+num1 = int(input("Enter a first number: "))
+num2 = int(input("Enter your second number:"))
+Sum = num1 + num2
+difference = num1 - num2
+print("The  sum of the number is: " ,  Sum)
+print("The difference of the number is" , difference)
---
+++ ProgramRectangle.py
@@ -0,0 +1,6 @@
+length = float(input("Enter a length: "))
+breadth = float(input("Enter a breadth: "))
+area = length * breadth
+parameter = 2*(length+breadth)
+print( "The  area of rectangle :" , float(area))
+print("The parameter is: " , float(parameter))
