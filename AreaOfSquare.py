c6c7060de2b9bbdf130fd2f145e5c00370d7d5ff
pawan_poudel_Devloper <poudelpawan770@gmail.com>
2026-04-01 06:36:43 +0545

    Add files via upload

---
+++ AreaOfSquare.py
@@ -0,0 +1,4 @@
+#  WPA to input side of  square and prints its area
+side = int(input("Enter a side of a square: "))
+area = side**2
+print("The area of square is :",area)
---
+++ CheckNum.py
@@ -0,0 +1,5 @@
+'''Write a program to input 2 int numbers a and b. print True if a is greater than or equal to b . IF not print False'''
+a = int(input("enter first num: "))
+b = int(input("enter second num: "))
+c = a>b
+print(c)
